d285abae8a4e8a0112262bf0b8288526389f25c2
Ewan Brain <ewan.brain@gmail.com>
2021-02-09 18:10:32 +0000

    second test passing

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void before(){
+        book = new Book("1984", "George Orwell", "Non-Fiction");
+    }
+
+    @Test
+    public void hasTitle(){
+        assertEquals("1984", book.getTitle());
+    }
 }
