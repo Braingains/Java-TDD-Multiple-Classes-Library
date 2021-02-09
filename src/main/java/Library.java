e94f24a11d52e2d6b66831a51fdaa2fffeb91a33
Ewan Brain <ewan.brain@gmail.com>
2021-02-09 18:02:50 +0000

    classes set up

---
+++ src/main/java/Library.java
@@ -0,0 +1,13 @@
+import javax.lang.model.type.ArrayType;
+import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book> collection;
+    private int capacity;
+
+    public Library(int capacity){
+        this.collection = new ArrayList<>();
+        this.capacity = capacity;
+    }
+}
