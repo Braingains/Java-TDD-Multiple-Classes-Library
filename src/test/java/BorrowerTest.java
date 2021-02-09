32d3cd02276c1999a6f67657bf185dd20284b369
Ewan Brain <ewan.brain@gmail.com>
2021-02-09 18:43:10 +0000

    added borrower class, eighth test passing

---
+++ src/test/java/BorrowerTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BorrowerTest {
+
+    private Borrower borrower;
+
+    @Before
+    public void before() {borrower = new Borrower("Winston Smith"); }
+
+    @Test
+    public void hasName(){
+        assertEquals("Winston Smith", borrower.getName());
+    }
+}
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -39,5 +39,5 @@ public class LibraryTest {
         assertEquals(false, tinyLibrary.addBookToCollection(book));
 
     }
-    
+
 }
